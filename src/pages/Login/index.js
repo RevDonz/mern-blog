33b262a9bed269580d7e2b1ab7c39e75fc03fd68
RevDonz <aprilio842@gmail.com>
2021-04-29 21:14:37 +0700

    Membuat Routing, Atomic Components, Register Page, Styling

---
+++ src/pages/Login/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className="main-page">
+            {/* <div className="left">
+                <img src={RegisterBg} alt="Register Img" className="bg-image" />
+            </div>
+            <div className="right">
+                <p className="title">Register</p>
+                <Input label="Full Name" placeholder="Full Name" />
+                <Gap height={18} />
+                <Input label="Email" placeholder="Email" />
+                <Gap height={18} />
+                <Input label="Password" placeholder="Password" />
+                <Gap height={50} />
+                <Button title="Register" />
+                <Gap height={100} />
+                <Link title="Kembali ke Login" />
+            </div> */}
+        </div>
+    )
+}
+
+export default Login
