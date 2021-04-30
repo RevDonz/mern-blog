95676f7d085710b96a4486131fb7b24c844776b5
RevDonz <aprilio842@gmail.com>
2021-04-30 14:52:07 +0700

    Styling Detail Blog

--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { RegisterBg } from '../../assets'
+import './detailblog.scss'
 
 const DetailBlog = () => {
     return (
-        <div>
-            <p>Detail Blog Page</p>
+        <div className="detail-blog-wrapper">
+            <img className="img-cover" src={RegisterBg} alt="thumb"/>
+            <p className="blog-title">Title Vlog</p>
+            <p className="blog-author">Author - Date Post</p>
+            <p className="blog-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque recusandae aliquam, quis adipisci est inventore mollitia esse natus quia soluta quaerat eum, deleniti necessitatibus! Cupiditate et eius corporis nihil beatae.</p>
         </div>
     )
 }
