080e2ab20af4eb8762612c0d852e5cfa6a99e81b
RevDonz <aprilio842@gmail.com>
2021-04-30 14:07:46 +0700

    Styling Halaman Create Blog post

---
+++ src/components/atoms/TextArea/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './textarea.scss'
+
+const index = ({...rest}) => {
+    return (
+        <div>
+            <textarea className="text-area" {...rest}>
+
+            </textarea>
+        </div>
+    )
+}
+
+export default index
